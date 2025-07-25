e5fbeda8eec0192acb4d68a0c6d873d7ad2ac092
firaolkiya <firaolkiya6@gmail.com>
2025-07-25 18:07:40 +0300

    data types

--- data_types.ts
+++ data_types.ts
@@ -43,6 +43,7 @@ function logMessage(message: string): void {
 
 
 // number
+// number can only represent upto 9007199254740991
 let myNumber: number = 42;
 let myHex: number = 0x2a; // hexadecimal
 let myBinary: number = 0b101010; // binary
@@ -60,5 +61,21 @@ let myFalseBoolean: boolean = false;
 
 
 // symbol
+// symbol is a type that represents a unique identifier, it is used to create unique object properties
 let mySymbol: symbol = Symbol("mySymbol");
 let myAnotherSymbol: symbol = Symbol("mySymbol"); // different symbol, even with the
+
+// same description
+// bigint
+// bigint is a built-in primitive type used to represent very large integers—numbers
+//  that are too large to be represented by the regular number type (which uses 64-bit 
+// floating point under the hood and can only safely represent 
+// integers between -(2^53 - 1) and 2^53 - 1).
+let myBigInt: bigint = 1234567890123456789012345678901234567890n; // bigint literal
+let myBigIntFromString: bigint = BigInt("1234567890123456789012345678901234567890"); // from string
+
+// object
+// object is a type that represents any non-primitive value, it is used to create objects
+let myObject = { name: "John", age: 30 };
+
+console.log(myObject.name); // accessing object property
